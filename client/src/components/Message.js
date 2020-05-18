a5204956b8c68d2b9ca55a5e9d68893c1adb7be5
Roman <romalicevich@gmail.com>
2020-05-19 00:42:38 +0300

    add state in messages

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -4,15 +4,18 @@ import iconPeople from '../images/icon-people.png'
 
 export default class Message extends React.Component {
   render () {
-    return(
+  	const mes = this.props.messages.map((i, index) => 
       <div className="container">
         <img src={iconPeople} className="icon"/>
         <table className="table">
-          <tr><td><p className="messager">Yulia</p></td>
+          <tr><td><p className="messager">{this.props.messages[index].username}</p></td>
           <td><p className="date">18:00 12.05.2020</p></td></tr>
         </table>
-        <p className="message">Hi bro</p>
+        <p className="message">{this.props.messages[index].text}</p>
       </div>
+    )
+    return (
+      mes
     )
   }
 }
--- client/src/components/Messages.js
+++ client/src/components/Messages.js
@@ -4,6 +4,16 @@ import iconPeople from '../images/icon-people.png'
 import Message from './Message'
 
 export default class Messages extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      messages : [
+        {username: 'Yulia', text: 'Hi bro', createdAt: '18:00 12.05.2020'},
+        {username: 'Yulia', text: 'Hi bro', createdAt: '18:00 12.05.2020'},
+        {username: 'Yulia', text: 'Hi bro', createdAt: '18:00 12.05.2020'},
+      ]
+    }
+  }
   render () {
     return(
       <div>
@@ -11,8 +21,9 @@ export default class Messages extends React.Component {
           <b className="main-font sets-peoples-of-chat"> ✩ Yulia</b>
         </div>
         <div className="chat-with-people">
-          <Message/>
-          <div className="container fild-for-message">
+          <Message
+            messages={this.state.messages}/>
+          <div className="fild-for-message">
             <input type="text" className="input-message"/>
           </div>
         </div>

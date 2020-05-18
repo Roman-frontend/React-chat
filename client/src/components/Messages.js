5feab6235bb6542df4f3a08db53405acff818d1c
Roman <romalicevich@gmail.com>
2020-05-18 21:44:41 +0300

    added component Message

--- client/src/components/Messages.js
+++ client/src/components/Messages.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import '../css/OpinShareCSS.css'
 import iconPeople from '../images/icon-people.png'
+import Message from './Message'
 
-export default class FieldMessage extends React.Component {
+export default class Messages extends React.Component {
   render () {
     return(
       <div>
@@ -10,20 +11,9 @@ export default class FieldMessage extends React.Component {
           <b className="main-font sets-peoples-of-chat"> ✩ Yulia</b>
         </div>
         <div className="chat-with-people">
-          <div className="container container-for-messages">
-            <img src={iconPeople} className="icon"/>
-            <div><p className="messager">Yulia</p></div>
-            <div><p className="time-of-send">18:00 12.05.2020</p></div>
-            <div><p className="message-from-people">Hi bro</p></div>
-          </div>
-          <div className="container container-for-messages">
-            <img src={iconPeople} className="icon"/>
-            <div><p className="messager">Yulia</p></div>
-            <div><p className="time-of-send">18:00 12.05.2020</p></div>
-            <div><p className="message-from-people">How are you?</p></div>
-          </div>
+          <Message/>
           <div className="container fild-for-message">
-            <input type="text" name="Message #general" className="input-message"/>
+            <input type="text" className="input-message"/>
           </div>
         </div>
       </div>
--- client/src/components/OpinShare.js
+++ client/src/components/OpinShare.js
@@ -4,6 +4,7 @@ import Search from './Search'
 import Profile from './Profile'
 import Messages from './Messages'
 import SetUser from './SetUser'
+import Message from './Message'
 
 export default class OpinShare extends React.Component {
   render() {

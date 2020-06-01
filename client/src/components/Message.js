74cdf8ed2b000cf9d9380c9e1db3479898eff951
Roman <romalicevich@gmail.com>
2020-06-01 17:29:55 +0300

    complite edit message with some litle help ))

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -2,18 +2,13 @@ import React, {useState} from 'react'
 import '../css/OpinShareCSS.css'
 import iconPeople from '../images/icon-people.png'
 
-export default function Message(messages, setMessages, inputRef) {
-
-  function editDate(date) {
-    if (date < 10) { return `0${date}`}
-    else { return date}
-  }
-
-  const sli = messages.slice(0, messages.length);
-  const newDate = new Date();
-  const date = `${editDate(newDate.getHours())}:${editDate(newDate.getMinutes())}
-    ${editDate(newDate.getDate())}.${editDate(newDate.getMonth())}.${newDate.getFullYear()}`; 
-  sli.unshift({username: 'Yulia', text: inputRef.current.value, createdAt: date},)
-  setMessages(sli);
-  inputRef.current.value = null
+export default function Message(props) {
+  return (
+    <div className="container">
+      <div className="icon"><img src={iconPeople}/></div>
+      <div className="messager"><p>{props.messages.username}</p></div>
+      <div className="date"><p>{props.messages.createdAt}</p></div>
+      <div className="message"><p>{props.messages.text}</p></div>
+    </div>
+    )
 }
--- client/src/components/Messages.js
+++ client/src/components/Messages.js
@@ -6,19 +6,23 @@ import Message from './Message'
 export default function Messages(props) {
   const inputRef = useRef(null);
   const [messages, setMessages] = useState([])
-  const mes = messages.map((i, index) => 
-    <div className="container">
-      <div className="icon"><img src={iconPeople}/></div>
-      <div className="messager"><p>{messages[index].username}</p></div>
-      <div className="date"><p>{messages[index].createdAt}</p></div>
-      <div className="message"><p>{messages[index].text}</p></div>
-    </div>
-  )
+
+  function editDate(date) {
+    if (date < 10) { return `0${date}`}
+    else { return date}
+  }
+
 
   function keyInput(event) {
     if (event.key === "Enter") {
       if (inputRef.current.value === "") {return}
-      Message(messages, setMessages, inputRef)
+      const sli = messages.slice(0, messages.length);
+      const newDate = new Date();
+      const date = `${editDate(newDate.getHours())}:${editDate(newDate.getMinutes())}
+        ${editDate(newDate.getDate())}.${editDate(newDate.getMonth())}.${newDate.getFullYear()}`; 
+      sli.unshift({username: 'Yulia', text: inputRef.current.value, createdAt: date},)
+      setMessages(sli);
+      inputRef.current.value = null
     }
   }
 
@@ -26,12 +30,20 @@ export default function Messages(props) {
     inputRef.current.focus()
   })
 
+  function renderMessages(messages) {
+    return messages.map((msg) => {
+      return <Message messages={msg}/>
+    })
+  }
+
   return(
     <div className="right-block">
       <div className="nick-people">
         <b className="main-font sets-peoples-of-chat"> ✩ Yulia</b>
       </div>
-      <div className="chat-with-people">{mes}</div>
+      <div className="chat-with-people">
+      {renderMessages(messages)}
+      </div>
       <div className="fild-for-message">
         <input type="text" className="input-message"
           ref={inputRef} onKeyUp={event => keyInput(event)}/>

7905c667ddbba051172009f9b5e49690d53a60ce
Roman <romanlicevich@gmail.com>
2020-08-20 13:39:33 +0300

    Refactored singInPage and TextField components (added Formik, Form, Field, ErrorMessages from formik library)

--- client/src/pages/SignInPage.js
+++ client/src/pages/SignInPage.js
@@ -1,38 +1,34 @@
 import React, {useContext, useEffect, useState, useRef} from 'react'
-import {useFormik} from 'formik'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 //https://github.com/jquense/yup  - Силка на додаткові методи yup
 import * as Yup from 'yup'
 import {Link} from 'react-router-dom'
 import {useHttp} from '../hooks/http.hook'
 import {AuthContext} from '../context/AuthContext'
 import {TextField} from '../components/TextField.js'
-import {useValidate} from '../hooks/validate.hook.js'
 
 export const SignInPage = () => {
-
+  const auth = useContext(AuthContext)
+  const {loading, request, error, clearError} = useHttp()
+  const formValues = {}
   const initialValues = {
     email: '',
     password: ''
   }
 
-  const onSubmit = values => {
+  useEffect(() => {
+    clearError()
+  }, [error, clearError])
+
+  const onSubmit = async values => {
+
+    try {
+      const emailPassword = { email: values.email, password: values.password }
+      const data = await request('/api/auth/login', 'POST', emailPassword)
+      auth.login(data.name, data.token, data.userId)
+    } catch (e) {console.error(e)}
+
     console.log('Form data ', values)
-  }
-
-  const validate = values => {
-    let errors = {}
-
-    if (!values.email) {
-      errors.email = 'Required'
-    } else if (
-      !(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email))
-    ) { errors.email = `Invalide email format`}
-
-    if (!values.password) {
-      errors.password = 'Required'
-    }
-
-    return errors
   }
 
   const validationSchema = Yup.object({
@@ -40,41 +36,14 @@ export const SignInPage = () => {
     password: Yup.string().required('Required')
   })
 
-  const formik = useFormik({
-    initialValues,
-    onSubmit,
-    validationSchema
-  })
-
-  console.log('form touched ', formik.touched)
-
-  const auth = useContext(AuthContext)
-  const {loading, request, error, clearError} = useHttp()
-  const {errors, validateEmail, validatePassword} = useValidate()
-  const emailRef = useRef(null);
-  const passwordRef = useRef(null);
-  const [correctForm, setCorrectForm] = useState({
-    email: undefined, password: undefined
-  })
-
-  useEffect(() => {
-    clearError()
-  }, [error, clearError])
-
-  const handleSubmit = async () => {
-/*    const validatedEmail = validateEmail(emailRef.current.value)
-    console.log('errors -', errors.email)
-    setCorrectForm({email: errors.email})*/
-    try {
-      const emailPassword = { email: emailRef.current.value, password: passwordRef.current.value }
-      const data = await request('/api/auth/login', 'POST', emailPassword)
-      auth.login(data.name, data.token, data.userId)
-    } catch (e) {}
-  }
-
   return (
-    <div className="auth-body">
-      <form className="auth-field" onSubmit={formik.handleSubmit}>
+    <Formik 
+      className="auth-body"
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      <Form className="auth-field">
         <span className="card-title">Авторизація</span>
 
         <TextField 
@@ -82,43 +51,28 @@ export const SignInPage = () => {
           placeholder="Введите email" 
           id="email"
           name="email" 
-          correctForm={correctForm.email} 
           type="email" 
-          inputRef={emailRef} 
-          onChange={formik.handleChange}
-          value={formik.values.email}
-          onBlur={formik.handleBlur}
         />
-        {formik.touched.email && formik.errors.email ? <div className='errors'>{formik.errors.email}</div> : null}
+        <ErrorMessage name='email' />
 
         <TextField 
           label="Password" 
           placeholder="Введите password" 
           id="password"
           name="password" 
-          correctForm={correctForm.password} 
           type="password" 
-          inputRef={passwordRef} 
-          value={formik.values.password}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
         />
-        {formik.touched.password && formik.errors.password ? <div className='errors'>{formik.errors.password}</div> : null}
+        <ErrorMessage name='password' />
 
         <div className="card-action">
-          <button
-            className="button-active"
-            type="submit"
-            onClick={handleSubmit}
-            disabled={loading}
-          >
+          <button className="button-active" type="submit" disabled={loading}>
             Ввійти
           </button>
           <Link  to={`/signUp`}>
             <button className="button-pasive">Зареєструватись</button>
           </Link>
         </div>
-      </form>
-    </div>
+      </Form>
+    </Formik>
   )
 }
